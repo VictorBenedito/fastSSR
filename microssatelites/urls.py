472e26a46a789072c96d45b06a3e7d3cf4a385a0
Victor Benedito <victor.benedito12@hotmail.com>
2023-02-01 11:04:31 -0300

    Projeto adicionado ao repositório

---
+++ microssatelites/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('result/', views.result, name='result'),
+    path('table/', views.person_list, name='result'),
+    path('upload/', views.handle_upload, name='handle_upload')
+    # path('processed/', views.processed, name='processed'),
+    # path('', views.index, name='index'),
+    # path('login', views.user_login, name='login'),
+    # Demo view
+	# path('demo/', views.demo_view, name='demo'),
+
+]
